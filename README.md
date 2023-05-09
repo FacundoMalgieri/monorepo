2127631f7e07e0aed23db15f5e6548dd0feef632
FacundoMalgieri <facundo.malgieri@gmail.com>
2023-05-09 01:17:13 -0300

    test Rate controller and README

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+### Install
+To run the project with docker follow this steps:
+* Clone this repository
+* Make sure docker is running
+* `cd monorepo`
+* `docker-compose up`
+* Navigate to http://localhost:3000
+
+If you want to run the project locally instead:
+* Clone this repository
+* `cd monorepo`
+* `yarn`
+* `yarn start`
+
+### Sample Wallets
  * Old wallet ` 0x63C61B20f52C05C0aaA6a9184Da0A77c8f5c6519`
 * Empty wallet `0x07B64fEFBc6a6231e869Ea6a835f6D14fC45Dc6D`
-* 
+* Couldn't find any wallet with balance that is not older than 1 year
+
---
+++ packages/backend/src/rate/rate.controller.spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { mock, MockProxy } from 'jest-mock-extended';
+
+import { RateController } from './rate.controller';
+import { RateService } from './rate.service';
+import { Rate } from './rate.entity';
+
+describe('RateController', () => {
+  let controller: RateController;
+  let rateService: MockProxy<RateService>;
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RateController],
+      providers: [
+        { provide: RateService, useFactory: () => mock(RateService) },
+      ],
+    }).compile();
+
+    controller = module.get<RateController>(RateController);
+    rateService = module.get<MockProxy<RateService>>(RateService);
+  });
+
+  describe('updateRate', () => {
+    it('should update rate and return updated rate', async () => {
+      const address = '0x1234567890123456789012345678901234567890';
+      const mockRate: Partial<Rate> = { address };
+      const updatedRate: Rate = { id: 1, address, eur: 100, usd: 120 };
+
+      jest.spyOn(rateService, 'update').mockResolvedValue(updatedRate);
+
+      const result = await controller.updateRate(mockRate);
+
+      expect(rateService.update).toHaveBeenCalledWith(mockRate);
+      expect(result).toEqual(updatedRate);
+    });
+  });
+});
